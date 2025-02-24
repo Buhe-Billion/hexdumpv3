abbccae84168f4fcc02e1486033b511f7a001f86
B.Billion <buhebillion@gmail.com>
2025-02-24 16:01:53 +0200

    first assembly

--- hexdumpv3.asm
+++ hexdumpv3.asm
@@ -1,1 +1,55 @@
 ;DESCRIPTION                :Witness the power of an %include
+;
+;
+
+SECTION .text
+
+%MACRO EXIT 2
+MOV RSP,RBP
+POP RBP
+
+MOV RAX,%1
+MOV RDI,%2
+SYSCALL
+%ENDMACRO
+
+%INCLUDE "textlib.asm"
+
+GLOBAL _start
+
+_start:
+      MOV RBP,RSP     ;Tis but for debugging
+
+      XOR R15,R15
+      XOR RSI,RSI
+      XOR RCX,RCX
+
+      CALL LOADBUFF
+      CMP R15,0
+      JBE EXIT
+
+SCAN:
+      XOR RAX,RAX
+      MOV AL,[BUFF+RCX]
+      MOV RDX,RSI
+      AND RDX,000000000000000Fh
+      CALL DUMPCHAR
+
+      INC RSI
+      INC RCX
+      CMP RCX,R15
+      JB .MODTEST
+      CALL LOADBUFF
+      CMP R15,0
+      JBE DONE
+
+.MODTEST:
+      TEST RSI,000000000000000Fh
+      JNZ SCAN
+      CALL PRINTLINE
+      CALL CLEARLINE
+      JMP SCAN
+
+DONE:
+      CALL PRINTLINE
+      EXIT 
